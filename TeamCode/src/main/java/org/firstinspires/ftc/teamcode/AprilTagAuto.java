375e37b172803661021a0c955e78c2ff8dab2570
ShivamS <100112804+ShivamTheShrimp@users.noreply.github.com>
2026-01-08 19:57:56 -0800

    auto camera test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagAuto.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.AprilTagWebcam;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+@Autonomous
+public class AprilTagAuto extends OpMode {
+
+    AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
+    Integer detectedTagID = null;
+
+    @Override
+    public void init() {
+
+        aprilTagWebcam.init(hardwareMap, telemetry);
+        telemetry.addLine("Camera initialized. Scanning for AprilTags...");
+        telemetry.update();
+    }
+
+    @Override
+    public void init_loop() {
+        // Continuously scan for tags while waiting to start
+        aprilTagWebcam.update();
+        for (AprilTagDetection tag : aprilTagWebcam.getDetectedTags()) {
+            // Save the first detected tag ID
+            detectedTagID = tag.id;
+            telemetry.addData("Detected Tag ID", detectedTagID);
+            break; // Stop after detecting one tag
+        }
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        // Use the saved tag ID during autonomous
+        if (detectedTagID != null) {
+            telemetry.addData("Saved Tag ID", detectedTagID);
+        } else {
+            telemetry.addLine("No tag detected yet.");
+        }
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        aprilTagWebcam.stop();
+    }
+}
